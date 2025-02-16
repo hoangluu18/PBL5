0f605d748f641ca2614996b407e5443eee38f455
unknown <thanhadp2402@gmail.com>
2025-02-16 18:54:48 +0700

    update gitignore file and add BE structure 16.02.2025

---
+++ BE/BE_Client/src/main/java/com/example/demo/BeClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeClientApplication.class, args);
+	}
+
+}
