f49804f13c6623e67137ae819332e375b7d3cd7e
unknown <thanhadp2402@gmail.com>
2025-03-14 22:18:53 +0700

    design product detail page

---
+++ FE/FE_Client/src/components/product_details/ProductDetailTab.tsx
@@ -0,0 +1,34 @@
+import { Table } from "antd";
+
+const columns = [
+    {
+        dataIndex: "label",
+        key: "label",
+        render: (text: string) => <strong>{text}</strong>,
+    },
+    {
+        dataIndex: "value",
+        key: "value",
+    },
+];
+
+const data = [
+    { key: "1", label: "CHIP NAME", value: "Apple M1 chip" },
+    { key: "2", label: "CPU CORE", value: "8 (4 performance and 4 efficiency)" },
+    { key: "3", label: "GPU CORE", value: "7" },
+    { key: "4", label: "NEURAL ENGINE", value: "16 cores" },
+];
+
+const ProductDetailTab = () => {
+    return (
+        <Table
+            columns={columns}
+            dataSource={data}
+            pagination={false}
+            showHeader={false}
+            style={{ width: "1000px" }}
+        />
+    );
+};
+
+export default ProductDetailTab;
