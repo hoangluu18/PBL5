382f14ef04e9bba12946370d48ccb73cbc106e86
thanh <thanhadp2402@gmail.com>
2025-04-03 16:55:19 +0700

    update header and vote review api

--- BE/WebParent/BE_Client/src/main/java/com/pbl5/client/repository/ReviewRepository.java
+++ BE/WebParent/BE_Client/src/main/java/com/pbl5/client/repository/ReviewRepository.java
@@ -1,8 +1,10 @@
 package com.pbl5.client.repository;
 
-import com.pbl5.common.entity.Review;
+import com.pbl5.common.entity.review.Review;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +14,10 @@ import java.util.List;
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
 
     @Query("SELECT r FROM Review r WHERE r.product.id = ?1")
-    public List<Review> findAllByProductId(Integer productId);
+    public Page<Review> findAllByProductId(Integer productId, Pageable pageable);
+
+    @Query("UPDATE Review r SET r.votes = (SELECT COUNT(*) FROM ReviewVote rv WHERE rv.review.id = ?1) " +
+            "WHERE r.id = ?1")
+    @Modifying
+    public void updateVoteCount(Integer reviewId);
 }
