66fb737facc8c8454885af21fdd7fce4804c822c
Lưu Việt Hoàng <luu186835@gmail.com>
2025-04-07 23:24:59 +0700

    feat: Implement order detail functionality with API integration and frontend updates

--- BE/WebParent/BE_Client/src/main/java/com/pbl5/client/service/OrderDetailService.java
+++ BE/WebParent/BE_Client/src/main/java/com/pbl5/client/service/OrderDetailService.java
@@ -1,5 +1,7 @@
 package com.pbl5.client.service;
 
+import com.pbl5.client.dto.CartProductDto;
+import com.pbl5.client.dto.order_detail.OrderDetailDto;
 import com.pbl5.common.entity.OrderDetail;
 
 import java.util.List;
@@ -8,4 +10,8 @@ public interface OrderDetailService {
     List<OrderDetail> findAll();
     OrderDetail save(OrderDetail orderDetail);
     void deleteById(Integer id);
+    boolean saveAll(List<OrderDetail> orderDetails);
+    List<CartProductDto> getOrderDetailByOrderId(Integer orderId);
+
+    OrderDetailDto getOrderDetailDto(Integer orderId);
 }
